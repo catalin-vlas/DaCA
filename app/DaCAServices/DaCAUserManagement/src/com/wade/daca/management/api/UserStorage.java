304ae562c78a3541a70eead86e186e3a5613e050
Barcan Virgil <ghitza9404@gmail.com>
2018-01-06 21:13:35 +0200

    Implement Add, Delete, Update for User.

--- app/DaCAServices/DaCAUserManagement/src/com/wade/daca/management/api/UserStorage.java
+++ app/DaCAServices/DaCAUserManagement/src/com/wade/daca/management/api/UserStorage.java
@@ -5,5 +5,8 @@ import com.wade.daca.management.dataobjects.User;
 public interface UserStorage {
 
     User getUserById(String id);
-
+    boolean addUser(User user);
+    boolean removeUser(User user);
+    boolean removeUser(String id);
+    boolean updateUser(String id, User user);
 }
