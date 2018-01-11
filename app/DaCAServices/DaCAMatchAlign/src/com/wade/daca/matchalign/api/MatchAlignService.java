26a9111a286074521e3089cd35d2ee2c06958641
Barcan Virgil <ghitza9404@gmail.com>
2018-01-11 21:52:47 +0200

    Add MatchAlignService

---
+++ app/DaCAServices/DaCAMatchAlign/src/com/wade/daca/matchalign/api/MatchAlignService.java
@@ -0,0 +1,9 @@
+package com.wade.daca.matchalign.api;
+
+public interface MatchAlignService {
+
+    // TODO: Do something that actually makes sense, not this!
+    public boolean match(String graph1, String graph2);
+    public boolean align(String graph1, String graph2);
+
+}
