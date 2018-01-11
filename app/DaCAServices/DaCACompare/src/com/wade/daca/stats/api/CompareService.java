66758ec0045f77e5b2fa3c12044e19d5f291e371
Barcan Virgil <ghitza9404@gmail.com>
2018-01-11 21:40:34 +0200

    Add CompareService

---
+++ app/DaCAServices/DaCACompare/src/com/wade/daca/stats/api/CompareService.java
@@ -0,0 +1,8 @@
+package com.wade.daca.stats.api;
+
+public interface CompareService {
+
+    // TODO: Do something that actually makes sense, not this!
+    public boolean compare(String graph1, String graph2);
+
+}
