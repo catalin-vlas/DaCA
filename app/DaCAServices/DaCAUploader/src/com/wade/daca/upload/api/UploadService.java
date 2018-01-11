10ba78856baa148c8692db6a4865cd54f6c079d6
Barcan Virgil <ghitza9404@gmail.com>
2018-01-11 20:59:37 +0200

    Add UploadService

---
+++ app/DaCAServices/DaCAUploader/src/com/wade/daca/upload/api/UploadService.java
@@ -0,0 +1,8 @@
+package com.wade.daca.upload.api;
+
+public interface UploadService {
+
+    // TODO: Do something that actually makes sense, not this!
+    public boolean upload(String filePath);
+
+}
