5e380da5f752e399d3974e8d2f65068fdbf538af
catalin.vlas <catalin.v@mail.ru>
2018-01-21 16:22:01 +0200

    Add DaCAUploader interface

--- app/DaCAServices/DaCAUploader/src/main/java/com/wade/daca/uploader/api/UploaderService.java
+++ app/DaCAServices/DaCAUploader/src/main/java/com/wade/daca/uploader/api/UploaderService.java
@@ -1,33 +1,14 @@
 package com.wade.daca.uploader.api;
 
-import com.wade.daca.uploader.storage.StorageFileNotFoundException;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 public interface UploaderService {
 
-    @GetMapping("/")
-    String listUploadedFiles(Model model) throws IOException;
+    @RequestMapping(value = "/upload/{namespaceId}/{url}", method = RequestMethod.PUT)
+    String addTriplesFromFile(@PathVariable("namespaceId") String namespace_id,@PathVariable("url") String url);
 
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    ResponseEntity<Resource> serveFile(@PathVariable String filename);
-
-    @PostMapping("/")
-    String handleFileUpload(@RequestParam("file") MultipartFile file,
-                            @RequestParam("namespace") String namespace,
-                            RedirectAttributes redirectAttributes);
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc);
+    @RequestMapping(value = "/upload/{namespaceId}", method = RequestMethod.PUT)
+    String addTriplesFromFile(@PathVariable("namespaceId") String namespace_id,@RequestParam("file")  MultipartFile file);
 }
--- app/DaCAServices/DaCAUploader/src/main/java/com/wade/daca/uploader/impl/UploaderServiceImpl.java
+++ app/DaCAServices/DaCAUploader/src/main/java/com/wade/daca/uploader/impl/UploaderServiceImpl.java
@@ -1,61 +1,35 @@
 package com.wade.daca.uploader.impl;
 
 import com.wade.daca.uploader.api.UploaderService;
-import com.wade.daca.uploader.storage.StorageFileNotFoundException;
-import com.wade.daca.uploader.storage.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 @Controller
 public class UploaderServiceImpl implements UploaderService {
 
-    private final StorageService storageService;
-
-    @Autowired
-    public UploaderServiceImpl(StorageService storageService) {
-        this.storageService = storageService;
-    }
-
-
     @Override
-    public String listUploadedFiles(Model model) throws IOException {
-        model.addAttribute("files", storageService.loadAll().map(
-                path -> MvcUriComponentsBuilder.fromMethodName(UploaderServiceImpl.class,
-                        "serveFile", path.getFileName().toString()).build().toString())
-                .collect(Collectors.toList()));
+    @RequestMapping(value = "/upload/{namespaceId}/{url}", method = RequestMethod.PUT)
+    public String addTriplesFromFile(@PathVariable("namespaceId") String namespaceId,@PathVariable("url") String url){
+        String result = "Success";
 
-        return "uploadForm";
+
+
+        return result;
     }
 
     @Override
-    public ResponseEntity<Resource> serveFile(String filename) {
+    @RequestMapping(value = "/upload/{namespaceId}", method = RequestMethod.PUT)
+    public String addTriplesFromFile(@PathVariable("namespaceId") String namespaceId,@RequestParam("file")  MultipartFile file){
+        String result = "Success";
 
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
+        
 
-    @Override
-    public String handleFileUpload(MultipartFile file, String namespace, RedirectAttributes redirectAttributes) {
-        storageService.store(file, namespace);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
-    }
-
-    @Override
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
+        return result;
     }
 }
+
+
