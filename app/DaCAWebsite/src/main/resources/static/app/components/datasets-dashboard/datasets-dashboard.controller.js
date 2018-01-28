603b56a01ab44b57f5c7d173945a54763215aa0e
Barcan Virgil <ghitza9404@gmail.com>
2018-01-28 17:40:06 +0200

    Add dataset visualization + SPARQL on website

--- app/DaCAWebsite/src/main/resources/static/app/components/datasets-dashboard/datasets-dashboard.controller.js
+++ app/DaCAWebsite/src/main/resources/static/app/components/datasets-dashboard/datasets-dashboard.controller.js
@@ -9,46 +9,49 @@
         });
 
 
-    DatasetsDashboardCtrl.$inject = [];
+    DatasetsDashboardCtrl.$inject = ['$http'];
 
-    function DatasetsDashboardCtrl() {
+    function DatasetsDashboardCtrl($http) {
         let vm = this;
+        let STATS_NAMESPACES_URL = "http://localhost:1997/stats/namespaces";
 
-        vm.datasets = [
-            {
-                'name': 'Species',
-                'size': '4MB',
-                'numberOfGraphs': 5,
-                'numberOfNodes': 1337,
-                'numberOfTriples': 4500,
-                'maximumTriples': 21,
-                'graphs': [
-                    {'name': 'Graph A'},
-                    {'name': 'Graph B'},
-                    {'name': 'Graph C'},
-                    {'name': 'Graph D'}
-                ]
-            },
-            {
-                'name': 'Antibiotics',
-                'size': '17MB',
-                'numberOfGraphs': 2,
-                'numberOfNodes': 666013,
-                'numberOfTriples': 10000007,
-                'maximumTriples': 15,
-                'graphs': [
-                    {'name': 'Graph 1'},
-                    {'name': 'Graph 2'},
-                    {'name': 'Graph 3'},
-                    {'name': 'Graph 5'},
-                    {'name': 'Graph 7'},
-                    {'name': 'Graph 4'}
-                ]
-            }
-        ];
+        vm.$onInit = function() {
+            getNamespaces().then(function success(response) {
+                vm.datasets = [];
+                for(let i = 0 ; i < response.data.length; i++) {
+                    vm.datasets.push({
+                        'name': response.data[i],
+                        'size': '4MB',
+                        'numberOfGraphs': 5,
+                        'numberOfNodes': 1337,
+                        'numberOfTriples': 4500,
+                        'maximumTriples': 21,
+                        'graphs': [
+                            {'name': 'Graph A'},
+                            {'name': 'Graph B'},
+                            {'name': 'Graph C'},
+                            {'name': 'Graph D'}
+                        ]
+                    });
+                }
+            }, function error(response) {
+                alert("Couldn't get namespaces");
+            })
+        };
 
         vm.setIndividualDataset = function(dataset) {
             vm.onSetIndividualDataset({'dataset': dataset});
         };
+
+        function getNamespaces() {
+            return $http({
+                method: "GET",
+                url: STATS_NAMESPACES_URL
+            }).then(function success(response) {
+                return response;
+            }, function error(response) {
+                return response;
+            });
+        }
     }
 })();
--- app/DaCAWebsite/src/main/resources/static/app/components/single-dataset/single-dataset.controller.js
+++ app/DaCAWebsite/src/main/resources/static/app/components/single-dataset/single-dataset.controller.js
@@ -6,13 +6,72 @@
         .controller('SingleDatasetCtrl', SingleDatasetCtrl);
 
 
-    SingleDatasetCtrl.$inject = [];
+    SingleDatasetCtrl.$inject = ['$http'];
 
-    function SingleDatasetCtrl() {
+    function SingleDatasetCtrl($http) {
         let vm = this;
+        let TRIPLES_URL = "http://localhost:1994/triples/{datasetName}";
+        let QUERY_URL = "http://localhost:1994/sparql/query";
 
-        vm.selectGraph = function(ev, graph) {
-            vm.selectedGraph = graph;
+        vm.$onInit = function() {
+            getTriples().then(function success(response) {
+                let triples = response.data;
+                let svg = d3.select("#svg-body").append("svg")
+                    .attr("width", 800)
+                    .attr("height", 600);
+
+                let force = d3.layout.force().size([800, 600]);
+
+                let graph = triplesToGraph(triples, svg);
+
+                update(graph, force, svg);
+            }, function error(response) {
+                alert("Couldn't get triples for dataset " + vm.dataset.name + ".");
+            });
         };
+
+        vm.submitQuery = function() {
+            executeQuery(vm.dataset.name, vm.query).then(function success(response) {
+                let triples = response.data;
+                d3.select("#svg-body").html("");
+                let svg = d3.select("#svg-body").append("svg")
+                    .attr("width", 800)
+                    .attr("height", 600);
+
+                let force = d3.layout.force().size([800, 600]);
+
+                let graph = triplesToGraph(triples, svg);
+
+                update(graph, force, svg);
+            }, function error(response) {
+                alert("Couldn't get triples for dataset " + vm.dataset.name + ".");
+            });
+        };
+
+        function getTriples() {
+            return $http({
+                method: "GET",
+                url: TRIPLES_URL.replace("{datasetName}", vm.dataset.name)
+            }).then(function success(response) {
+                return response;
+            }, function error(response) {
+                return response;
+            });
+        }
+
+        function executeQuery(datasetName, query) {
+            return $http({
+                method: "GET",
+                url: QUERY_URL,
+                params: {
+                    'namespaceId': datasetName,
+                    'query': query
+                }
+            }).then(function success(response) {
+                return response;
+            }, function error(response) {
+                return response;
+            });
+        }
     }
 })();
